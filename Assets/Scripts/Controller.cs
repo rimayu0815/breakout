e5854250460c533a551ed16aa376bc25f5f9da63
rimayu0815 <rikussyuu@gmail.com>
2020-10-24 17:10:37 +0900

    プレイヤーの挙動を制限中

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Xml.Xsl;
 using UnityEngine;
 
-public class Controller : MonoBehaviour
-{
+public class Controller : MonoBehaviour 
+{ 
+
+    GameObject player;
+    GameObject kabeLeft;
+    GameObject kabeRight;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("player");
+        kabeLeft = GameObject.Find("kabe left");
+        kabeRight = GameObject.Find("kabe right");
     }
 
     // Update is called once per frame
@@ -23,5 +29,8 @@ public class Controller : MonoBehaviour
         {
             transform.position -= transform.forward * 0.1f;
         }
+        float posZ = Mathf.Clamp(transform.position.z, kabeLeft.transform.position.z, kabeRight.transform.position.z);
+        transform.position = new Vector3(transform.position.x, transform.position.y, posZ); 
+                                    //Mathf.Clamp(player.transform.position.z, kabeLeft.transform.position.z, kabeRight.transform.position.z));
     }
 }
