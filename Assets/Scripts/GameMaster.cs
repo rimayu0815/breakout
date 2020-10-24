ca584043ea12a2c0da1426f3b5f01254c69ec8b0
rimayu0815 <rikussyuu@gmail.com>
2020-10-24 15:58:37 +0900

    初回コミット

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMaster : MonoBehaviour
+{
+    public int boxNum;
+    public float nowTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nowTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nowTime += Time.deltaTime;
+        if(boxNum <=0)
+        {
+            GameOver(nowTime.ToString("F0")+"秒でクリアできた！"); //<=======秒数をstring型にキャストとして引数へ
+        }
+    }
+
+    public void GameOver(string resultMessage)
+    {//<=======引数を持たせた
+        DataSender.resultMessage = resultMessage; //<======受け取った引数をstatic変数へ格納
+        SceneManager.LoadScene("Result");
+    }
+}
